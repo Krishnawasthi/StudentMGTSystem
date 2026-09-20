e9eb3c58cd7dd8574af6886e1e5cb4ee12cc9cd9
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-20 19:24:53 +0530

    feat: Define StudentService interface

---
+++ src/com/student/mgt/service/StudentService.java
@@ -0,0 +1,18 @@
+﻿package com.student.mgt.service;
+
+import com.student.mgt.exception.DuplicateStudentException;
+import com.student.mgt.exception.StudentNotFoundException;
+import com.student.mgt.model.Course;
+import com.student.mgt.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    Student registerStudent(String rollNo, String name, String email, Course course) throws DuplicateStudentException;
+    Student getStudentById(String id) throws StudentNotFoundException;
+    Student getStudentByRollNo(String rollNo) throws StudentNotFoundException;
+    List<Student> getAllStudents();
+    List<Student> getStudentsByCourse(Course course);
+    void addMarks(String studentId, double marks) throws StudentNotFoundException;
+    void deleteStudent(String id) throws StudentNotFoundException;
+}
