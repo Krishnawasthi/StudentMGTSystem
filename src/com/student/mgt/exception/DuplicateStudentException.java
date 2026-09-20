ca0b2bdfe12859a4a648fbf2e74ec28c62fe8598
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-20 19:24:52 +0530

    feat: Add custom exception classes for error handling

---
+++ src/com/student/mgt/exception/DuplicateStudentException.java
@@ -0,0 +1,7 @@
+﻿package com.student.mgt.exception;
+
+public class DuplicateStudentException extends Exception {
+    public DuplicateStudentException(String message) {
+        super(message);
+    }
+}
