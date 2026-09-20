73292c7a3de90f2ad028ccf08c11738d6cb13f8e
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-20 19:24:53 +0530

    feat: Create GradeCalculatorUtil for GPA and grade calculation

---
+++ src/com/student/mgt/util/GradeCalculatorUtil.java
@@ -0,0 +1,26 @@
+﻿package com.student.mgt.util;
+
+import com.student.mgt.model.Grade;
+import com.student.mgt.model.Student;
+
+import java.util.List;
+
+public class GradeCalculatorUtil {
+
+    public static Grade calculateGrade(Student student) {
+        double avg = student.getAverageMarks();
+        return Grade.fromMarks(avg);
+    }
+
+    public static double calculateGPA(Student student) {
+        List<Double> marks = student.getMarks();
+        if (marks == null || marks.isEmpty()) return 0.0;
+        
+        double totalGpa = 0.0;
+        for (double mark : marks) {
+            Grade g = Grade.fromMarks(mark);
+            totalGpa += g.getGpaValue();
+        }
+        return totalGpa / marks.size();
+    }
+}
