25309a67c00c87e0748857af1a8d3f6d805a4f07
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-20 19:24:54 +0530

    feat: Add CsvExporter utility for exporting student data to CSV

---
+++ src/com/student/mgt/util/CsvExporter.java
@@ -0,0 +1,28 @@
+﻿package com.student.mgt.util;
+
+import com.student.mgt.model.Student;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CsvExporter {
+    public static void exportStudentsToCsv(List<Student> students, String filename) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write("ID,Roll No,Name,Email,Course,Avg Marks,GPA,Attendance(%)\n");
+            for (Student s : students) {
+                double gpa = GradeCalculatorUtil.calculateGPA(s);
+                writer.write(String.format("%s,%s,\"%s\",%s,%s,%.2f,%.2f,%.1f\n",
+                        s.getStudentId(),
+                        s.getRollNo(),
+                        s.getName(),
+                        s.getEmail(),
+                        s.getCourse().getDisplayName(),
+                        s.getAverageMarks(),
+                        gpa,
+                        s.getAttendancePercentage()));
+            }
+        }
+    }
+}
