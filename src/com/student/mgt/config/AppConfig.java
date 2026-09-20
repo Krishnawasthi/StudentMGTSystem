13df9a64d365c3af973e95a0e3678535f7e38fb5
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-20 19:24:54 +0530

    feat: Create AppConfig class for application configuration

---
+++ src/com/student/mgt/config/AppConfig.java
@@ -0,0 +1,7 @@
+﻿package com.student.mgt.config;
+
+public class AppConfig {
+    public static final String DATA_FILE_PATH = "students_data.txt";
+    public static final String APP_NAME = "Core Java Student Management System";
+    public static final String VERSION = "1.0.0";
+}
