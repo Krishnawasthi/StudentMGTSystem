429633beafbb80023034cc6f2837bcfb4ca13b16
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-22 23:29:27 +0530

    refactor(config): Add configurable application parameters and operational constants to AppConfig

--- src/com/student/mgt/config/AppConfig.java
+++ src/com/student/mgt/config/AppConfig.java
@@ -3,5 +3,8 @@ package com.student.mgt.config;
 public class AppConfig {
     public static final String DATA_FILE_PATH = "students_data.txt";
     public static final String APP_NAME = "Core Java Student Management System";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.2.0";
+    public static final double MIN_ATTENDANCE_THRESHOLD = 75.0;
+    public static final int DEFAULT_TOP_PERFORMERS_LIMIT = 5;
+    public static final String LOG_FILE_PATH = "student_mgt.log";
 }
