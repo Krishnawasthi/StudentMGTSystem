857ae950eb55407e72bea2035d2af49c9d3aa726
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-22 23:02:15 +0530

    feat(exception): Introduce custom InvalidGradeException and StorageException

---
+++ src/com/student/mgt/exception/InvalidGradeException.java
@@ -0,0 +1,7 @@
+package com.student.mgt.exception;
+
+public class InvalidGradeException extends RuntimeException {
+    public InvalidGradeException(String message) {
+        super(message);
+    }
+}
