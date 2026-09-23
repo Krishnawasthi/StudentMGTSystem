75ed8ae9ef8fbe44eda0ef2610b38c73d27aba58
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-23 20:08:34 +0530

    refactor(config): Expand AppConfig with backup folder path and table styling settings

--- src/com/student/mgt/config/AppConfig.java
+++ src/com/student/mgt/config/AppConfig.java
@@ -3,8 +3,11 @@ package com.student.mgt.config;
 public class AppConfig {
     public static final String DATA_FILE_PATH = "students_data.txt";
     public static final String APP_NAME = "Core Java Student Management System";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.5.0";
     public static final double MIN_ATTENDANCE_THRESHOLD = 75.0;
     public static final int DEFAULT_TOP_PERFORMERS_LIMIT = 5;
     public static final String LOG_FILE_PATH = "student_mgt.log";
+    public static final String TEACHER_DATA_FILE = "teachers_data.dat";
+    public static final String BACKUP_DIR = "backups";
+    public static final int DEFAULT_MAX_COURSE_CAPACITY = 60;
 }
